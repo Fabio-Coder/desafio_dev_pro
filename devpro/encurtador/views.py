e5f8b7b67156f61238db0ed76644094567fa0968
Fabio-Coder <0xfab1015@gmail.com>
2021-05-19 10:16:36 -0300

    3º dia do desafio. Incluidos templates HTML e inicio. Arquivos dinâmicos gerados.

--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -8,3 +8,12 @@ from devpro.encurtador.models import UrlRedirect
 def redirecionar(requisicao, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
     return redirect(url_redirect.destino)
+
+def relatorios(requisicao, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    url_reduzida = requisicao.build_absolute_uri(f'/{slug}')
+    contexto = {
+        'reduce' : url_redirect,
+        'url_reduzida' : url_reduzida,
+    }
+    return render(requisicao, 'encurtador/reduce.html', contexto)
