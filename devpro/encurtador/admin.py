21b7a3fc8043ab9ba3a6887ab9ccc9c18c1b6945
Fabio-Coder <0xfab1015@gmail.com>
2021-05-21 10:13:31 -0300

    Incluido novo modelo para os logs no banco de dados.

--- devpro/encurtador/admin.py
+++ devpro/encurtador/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from devpro.encurtador.models import UrlRedirect
+from devpro.encurtador.models import UrlRedirect, UrlLog
 
 
 @admin.register(UrlRedirect)
 class UrlRedirect(admin.ModelAdmin):
     list_display = ('destino', 'slug', 'criado_em', 'atualizado_em')
+
+
+@admin.register(UrlLog)
+class UrlLogAdmin(admin.ModelAdmin):
+    list_display = ('origem', 'user_agent', 'host', 'ip', 'url_redirect')
+
+    def has_change_permission(self, request, obj=None):
+        return False
--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -2,18 +2,25 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from devpro.encurtador.models import UrlRedirect
+from devpro.encurtador.models import UrlRedirect, UrlLog
 
-
-def redirecionar(requisicao, slug):
-    url_redirect = UrlRedirect.objects.get(slug=slug)
-    return redirect(url_redirect.destino)
 
 def relatorios(requisicao, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
     url_reduzida = requisicao.build_absolute_uri(f'/{slug}')
     contexto = {
-        'reduce' : url_redirect,
-        'url_reduzida' : url_reduzida,
+        'reduce': url_redirect,
+        'url_reduzida': url_reduzida,
     }
     return render(requisicao, 'encurtador/reduce.html', contexto)
+
+
+def redirecionar(requisicao, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    UrlLog.objects.create(
+        origem=requisicao.META.get('HTTP_REFERER'),
+        user_agent = requisicao.META.get('HTTP_USER_AGENT'),
+        host = requisicao.META.get('HTTP_HOST'),
+        ip = requisicao.META.get('REMOTE_ADDR'),
+    )
+    return redirect(url_redirect.destino)
